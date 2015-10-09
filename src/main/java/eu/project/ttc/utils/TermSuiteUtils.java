f5a5829cb2bbe889062664f5090f528c57cd4bb3
Damien Cram <damien.cram@univ-nantes.fr>
2015-10-09 12:22:52 +0200

    Removed warning

--- src/main/java/eu/project/ttc/utils/TermSuiteUtils.java
+++ src/main/java/eu/project/ttc/utils/TermSuiteUtils.java
@@ -35,7 +35,6 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.zip.ZipInputStream;
 
-import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.mutable.MutableInt;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
