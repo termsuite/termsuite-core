1aa7f1eba7f18c7acef02f87aa65ec4f9bc26a63
Damien Cram <damien.cram@univ-nantes.fr>
2016-06-14 17:56:15 +0200

    Do not do native composition if the composition is already set

--- src/main/java/eu/project/ttc/engines/morpho/CompostAE.java
+++ src/main/java/eu/project/ttc/engines/morpho/CompostAE.java
@@ -183,6 +183,14 @@ public class CompostAE extends JCasAnnotator_ImplBase {
 			if(cnt%observingStep == 0) {
 				observer.work(observingStep);
 			}
+			
+			/*
+			 * Do not do native morphology splitting 
+			 * if a composition already exists.
+			 */
+			if(word.isCompound())
+				continue;
+			
 			Map<Segmentation, Double> scores = computeScores(word.getLemma());
 			if(scores.size() > 0) {
 				float bestScore = 0;
