a6bc548455adb7b7c48306e314810ee4b39bbfdb
Simon Meoni <simonmeoni@aol.com>
2016-06-27 12:01:18 +0200

    add word annotation extraction during the deserialization of the TOA but there are some bugs (need to fix it)

--- src/main/java/eu/project/ttc/readers/TermSuiteJsonCasDeserializer.java
+++ src/main/java/eu/project/ttc/readers/TermSuiteJsonCasDeserializer.java
@@ -6,14 +6,14 @@ import eu.project.ttc.types.TermOccAnnotation;
 import eu.project.ttc.types.WordAnnotation;
 import org.apache.uima.cas.CAS;
 import org.apache.uima.cas.CASException;
-import org.apache.uima.cas.FeatureStructure;
-import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.cas.FSArray;
 import org.apache.uima.jcas.cas.StringArray;
-import org.apache.uima.jcas.cas.StringArray_Type;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Collection;
+import java.util.NavigableMap;;
+import java.util.TreeMap;
 
 import static eu.project.ttc.readers.JsonCasConstants.*;
 
@@ -24,6 +24,7 @@ public class TermSuiteJsonCasDeserializer {
     public static void deserialize(InputStream inputStream, CAS cas) {
         try {
 
+
             JsonFactory factory = new JsonFactory();
             JsonParser parser = factory.createParser(inputStream);
             JsonToken token;
@@ -31,6 +32,7 @@ public class TermSuiteJsonCasDeserializer {
             SourceDocumentInformation sdi = (SourceDocumentInformation) cas.createAnnotation(cas.getJCas().getCasType(SourceDocumentInformation.type), 0, 0);
             WordAnnotation wa = (WordAnnotation) cas.createAnnotation(cas.getJCas().getCasType(WordAnnotation.type), 0, 0);
             TermOccAnnotation toa = (TermOccAnnotation) cas.createAnnotation(cas.getJCas().getCasType(TermOccAnnotation.type), 0, 0);
+            NavigableMap<Integer, WordAnnotation> waTreeMap = new TreeMap<>();
             boolean inSdi = false;
             boolean inWa = false;
             boolean inToa = false;
@@ -55,6 +57,7 @@ public class TermSuiteJsonCasDeserializer {
                             inWa = false;
                         }
                         else if (token == JsonToken.END_OBJECT) {
+                            waTreeMap.put(wa.getBegin(),(WordAnnotation) wa.clone());
                             wa.addToIndexes();
                             wa = (WordAnnotation) cas.createAnnotation(cas.getJCas().getCasType(WordAnnotation.type), 0, 0);
                         }
@@ -66,6 +69,7 @@ public class TermSuiteJsonCasDeserializer {
                             inToa = false;
                         }
                         else if (token == JsonToken.END_OBJECT) {
+                            FillWords(toa,waTreeMap,cas);
                             toa.addToIndexes();
                             toa = (TermOccAnnotation) cas.createAnnotation(cas.getJCas().getCasType(TermOccAnnotation.type), 0, 0);
                         }
@@ -104,6 +108,19 @@ public class TermSuiteJsonCasDeserializer {
             e.printStackTrace();
         }
     }
+
+    private static void FillWords(TermOccAnnotation toa, NavigableMap<Integer, WordAnnotation> waTreeMap, CAS cas) throws CASException {
+        int begin = toa.getBegin();
+        int end = toa.getEnd();
+        Collection<WordAnnotation> subWaCollection = waTreeMap.subMap(begin,true,end,true).values();
+        WordAnnotation[] waArray = subWaCollection.toArray(new WordAnnotation[subWaCollection.size()]);
+        FSArray fs = (FSArray) cas.createArrayFS(subWaCollection.size());
+        for (int i = 0; i < waArray.length; i++){
+            fs.set(i,(WordAnnotation) waArray[i].clone());
+        }
+        toa.setWords(fs);
+    }
+
     private static void FillWordAnnotations(JsonParser parser, JsonToken token, WordAnnotation wa) throws IOException {
         if (token.equals(JsonToken.FIELD_NAME)){
             switch (parser.getCurrentName()){
