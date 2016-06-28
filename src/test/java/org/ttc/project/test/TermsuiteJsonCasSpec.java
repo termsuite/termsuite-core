879f9b7b5ab2db22268d59cf591a4af332778c30
Damien Cram <damien.cram@univ-nantes.fr>
2016-06-28 11:55:48 +0200

    Changed test file name (ends with *Spec) and the form of some assertions

--- src/test/java/org/ttc/project/test/TermsuiteJsonCasSpec.java
+++ src/test/java/org/ttc/project/test/TermsuiteJsonCasSpec.java
@@ -21,11 +21,13 @@
  *******************************************************************************/
 package org.ttc.project.test;
 
-import eu.project.ttc.readers.TermSuiteJsonCasDeserializer;
-import eu.project.ttc.readers.TermSuiteJsonCasSerializer;
-import eu.project.ttc.types.SourceDocumentInformation;
-import eu.project.ttc.types.TermOccAnnotation;
-import eu.project.ttc.types.WordAnnotation;
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import org.apache.uima.UIMAException;
 import org.apache.uima.cas.FSIterator;
 import org.apache.uima.fit.factory.JCasFactory;
@@ -33,30 +35,29 @@ import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.cas.FSArray;
 import org.apache.uima.jcas.cas.StringArray;
 import org.apache.uima.jcas.tcas.Annotation;
-import org.junit.*;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import org.ttc.project.TestUtil;
 
-import java.io.*;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import eu.project.ttc.readers.TermSuiteJsonCasDeserializer;
+import eu.project.ttc.readers.TermSuiteJsonCasSerializer;
+import eu.project.ttc.types.SourceDocumentInformation;
+import eu.project.ttc.types.TermOccAnnotation;
+import eu.project.ttc.types.WordAnnotation;
 
 /**
  * Created by Simon Meoni on 24/06/16.
  */
-public class TermsuiteJsonCas{
+public class TermsuiteJsonCasSpec {
 
     private File jsonExpectedFile;
     private File jsonResFile;
     private JCas jCas = JCasFactory.createJCas();
-    private JCas jCasExpected = JCasFactory.createJCas();
-    Writer writer;
+    private JCas fixtureCas = JCasFactory.createJCas();
     FileInputStream fis;
-    public TermsuiteJsonCas() throws UIMAException {
+    public TermsuiteJsonCasSpec() throws UIMAException {
     }
 
     @Rule
@@ -68,11 +69,10 @@ public class TermsuiteJsonCas{
         jsonExpectedFile = new File("src/test/resources/org/project/ttc/test/termsuite/json/cas/test.json");
         jsonResFile = temporaryFolder.newFile("test1.json");
 
-        writer = new FileWriter(jsonResFile);
         fis = new FileInputStream(jsonExpectedFile);
 
-        SourceDocumentInformation sdi = (SourceDocumentInformation) jCasExpected.getCas()
-                .createAnnotation(jCasExpected.getCasType(SourceDocumentInformation.type), 0, 37);
+        SourceDocumentInformation sdi = (SourceDocumentInformation) fixtureCas.getCas()
+                .createAnnotation(fixtureCas.getCasType(SourceDocumentInformation.type), 0, 37);
         sdi.setUri("test.json");
         sdi.setOffsetInSource(0);
         sdi.setDocumentIndex(1);
@@ -82,8 +82,8 @@ public class TermsuiteJsonCas{
         sdi.setLastSegment(true);
         sdi.addToIndexes();
 
-        WordAnnotation wa = (WordAnnotation) jCasExpected.getCas()
-                .createAnnotation(jCasExpected.getCasType(WordAnnotation.type), 0, 2);
+        WordAnnotation wa = (WordAnnotation) fixtureCas.getCas()
+                .createAnnotation(fixtureCas.getCasType(WordAnnotation.type), 0, 2);
         wa.setCategory("article");
         wa.setLemma("le");
         wa.setStem("le");
@@ -98,8 +98,8 @@ public class TermsuiteJsonCas{
         wa.setPerson("test");
         wa.addToIndexes();
 
-        wa = (WordAnnotation) jCasExpected.getCas()
-                .createAnnotation(jCasExpected.getCasType(WordAnnotation.type), 3, 15);
+        wa = (WordAnnotation) fixtureCas.getCas()
+                .createAnnotation(fixtureCas.getCasType(WordAnnotation.type), 3, 15);
         wa.setCategory("noun");
         wa.setLemma("recouvrement");
         wa.setStem("recouvr");
@@ -112,8 +112,8 @@ public class TermsuiteJsonCas{
         wa.setPerson("test");
         wa.addToIndexes();
 
-        WordAnnotation wa1 = (WordAnnotation) jCasExpected.getCas()
-                .createAnnotation(jCasExpected.getCasType(WordAnnotation.type), 16, 21);
+        WordAnnotation wa1 = (WordAnnotation) fixtureCas.getCas()
+                .createAnnotation(fixtureCas.getCasType(WordAnnotation.type), 16, 21);
         wa1.setCategory("adjective");
         wa1.setLemma("total");
         wa1.setStem("tot");
@@ -131,14 +131,14 @@ public class TermsuiteJsonCas{
         wa1.setLabels("label");
         wa1.addToIndexes();
 
-        TermOccAnnotation toa = (TermOccAnnotation) jCasExpected.getCas()
-                .createAnnotation(jCasExpected.getCasType(TermOccAnnotation.type),3,21);
+        TermOccAnnotation toa = (TermOccAnnotation) fixtureCas.getCas()
+                .createAnnotation(fixtureCas.getCasType(TermOccAnnotation.type),3,21);
 
 
-        StringArray stringArray = new StringArray(jCasExpected, 2);
+        StringArray stringArray = new StringArray(fixtureCas, 2);
         stringArray.set(0,"N");
         stringArray.set(1,"A");
-        FSArray fs = (FSArray) jCasExpected.getCas().createArrayFS(2);
+        FSArray fs = (FSArray) fixtureCas.getCas().createArrayFS(2);
         fs.set(0,wa);
         fs.set(1,wa1);
         toa.setPattern(stringArray);
@@ -147,7 +147,7 @@ public class TermsuiteJsonCas{
         toa.setWords(fs);
         toa.addToIndexes();
 
-        jCasExpected.setDocumentText("le recouvrement total");
+        fixtureCas.setDocumentText("le recouvrement total");
 
 
     }
@@ -156,10 +156,10 @@ public class TermsuiteJsonCas{
     public void SerializationTest() throws IOException {
 
         //Test Out File
-        TermSuiteJsonCasSerializer.serialize(writer,jCasExpected);
+        TermSuiteJsonCasSerializer.serialize(new FileWriter(jsonResFile),fixtureCas);
         final String baseFile = TestUtil.readFile(jsonExpectedFile);
         final String resFile = TestUtil.readFile(jsonResFile);
-        assertEquals("this two files must be equals", baseFile,resFile);
+        assertEquals(baseFile,resFile);
     }
 
     @Test
@@ -167,28 +167,28 @@ public class TermsuiteJsonCas{
         TermSuiteJsonCasDeserializer.deserialize(fis, jCas.getCas());
 
         //Test Sdi
-        assertEquals("Sdi must be equals",jCasExpected.getAnnotationIndex(SourceDocumentInformation.type).iterator().next().toString(),
+        assertEquals(fixtureCas.getAnnotationIndex(SourceDocumentInformation.type).iterator().next().toString(),
                 jCas.getAnnotationIndex(SourceDocumentInformation.type).iterator().next().toString());
 
         //Test wordAnnotation
-        FSIterator<Annotation> itWaEx = jCasExpected.getAnnotationIndex(WordAnnotation.type).iterator();
+        FSIterator<Annotation> itWaEx = fixtureCas.getAnnotationIndex(WordAnnotation.type).iterator();
         FSIterator<Annotation> itWa = jCas.getAnnotationIndex(WordAnnotation.type).iterator();
         while (itWa.hasNext()){
-            assertEquals("WordAnnotations must be equals",itWaEx.next().toString(),itWa.next().toString());
+            assertEquals(itWaEx.next().toString(),itWa.next().toString());
         }
-        assertEquals("TermOccAnnotations must be equals",
-                jCasExpected.getAnnotationIndex(TermOccAnnotation.type).iterator().next().toString(),
+        assertEquals(
+        		fixtureCas.getAnnotationIndex(TermOccAnnotation.type).iterator().next().toString(),
                 jCas.getAnnotationIndex(TermOccAnnotation.type).iterator().next().toString());
 
         //Test termOccAnnotation
-        TermOccAnnotation toaExpected = (TermOccAnnotation) jCasExpected.getAnnotationIndex(TermOccAnnotation.type).iterator().next();
+        TermOccAnnotation toaExpected = (TermOccAnnotation) fixtureCas.getAnnotationIndex(TermOccAnnotation.type).iterator().next();
         TermOccAnnotation toa = (TermOccAnnotation) jCas.getAnnotationIndex(TermOccAnnotation.type).iterator().next();
         for (int i = 0; i < toa.getWords().size(); i++){
-            assertEquals("TermOccAnnotation Words must be equals",toaExpected.getWords().get(i).toString(),toa.getWords().get(i).toString());
+            assertEquals(toaExpected.getWords().get(i).toString(),toa.getWords().get(i).toString());
         }
 
         //Test covered Text
-        assertEquals("Document Text must be equals",jCasExpected.getDocumentText(), jCas.getDocumentText());
+        assertEquals("le recouvrement total", jCas.getDocumentText());
     }
 
 }
